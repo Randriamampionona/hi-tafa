f85e852573c5cce83126acc1e48eee3e5ddb714b
Tooj Rtn <tojorandriaII474@gmail.com>
2023-07-10 16:38:27 +0300

    Adding ads

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ const MyDocument = () => {
 			<Head>
 				<meta name="description" content="'Ndao hi tafa!" />
 				<NextScript />
+		                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5906051574535378"
+     crossorigin="anonymous"></script>
 			</Head>
 
 			<body>
